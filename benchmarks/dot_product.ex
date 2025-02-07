88af1e346fbe35fda44ec6af56e939c0cdb6941c
ardubois <ardubois@github.com>
2025-02-07 14:52:47 -0300

    teste

--- benchmarks/dot_product.ex
+++ benchmarks/dot_product.ex
@@ -1,7 +1,7 @@
-require Hok
+require PolyHok
 #Nx.default_backend(EXLA.Backend)
 #import Nx
-Hok.defmodule_jit DP do
+PolyHok.defmodule_jit DP do
 include CAS
   defk map_2kernel(a1,a2,a3,size,f) do
     id = blockIdx.x * blockDim.x + threadIdx.x
@@ -11,30 +11,30 @@ include CAS
   end
   def map2(t1,t2,func) do
 
-    {l,c} = Hok.get_shape_gnx(t1)
-    type = Hok.get_type_gnx(t2)
+    {l,c} = PolyHok.get_shape_gnx(t1)
+    type = PolyHok.get_type_gnx(t2)
      size = l*c
-     result_gpu = Hok.new_gnx(l,c, type)
+     result_gpu = PolyHok.new_gnx(l,c, type)
 
       threadsPerBlock = 256;
       numberOfBlocks = div(size + threadsPerBlock - 1, threadsPerBlock)
 
-      Hok.spawn_jit(&DP.map_2kernel/5,{numberOfBlocks,1,1},{threadsPerBlock,1,1},[t1,t2,result_gpu,size,func])
+      PolyHok.spawn_jit(&DP.map_2kernel/5,{numberOfBlocks,1,1},{threadsPerBlock,1,1},[t1,t2,result_gpu,size,func])
 
 
       result_gpu
   end
   def reduce(ref, initial, f) do
 
-     {l,c} = Hok.get_shape_gnx(ref)
-     type = Hok.get_type_gnx(ref)
+     {l,c} = PolyHok.get_shape_gnx(ref)
+     type = PolyHok.get_type_gnx(ref)
      size = l*c
-      result_gpu  = Hok.new_gnx(Nx.tensor([[0]] , type: type))
+      result_gpu  = PolyHok.new_gnx(Nx.tensor([[0]] , type: type))
 
       threadsPerBlock = 256
       blocksPerGrid = div(size + threadsPerBlock - 1, threadsPerBlock)
       numberOfBlocks = blocksPerGrid
-      Hok.spawn_jit(&DP.reduce_kernel/4,{numberOfBlocks,1,1},{threadsPerBlock,1,1},[ref, initial,result_gpu, f, size])
+      PolyHok.spawn_jit(&DP.reduce_kernel/4,{numberOfBlocks,1,1},{threadsPerBlock,1,1},[ref, initial,result_gpu, f, size])
       result_gpu
   end
   defk reduce_kernel(a, initial, ref4, f,n) do
@@ -77,7 +77,7 @@ include CAS
   def replicate(n, x), do: (for _ <- 1..n, do: x)
 end
 
-#Hok.include [DP]
+#PolyHok.include [DP]
 
 [arg] = System.argv()
 
@@ -87,34 +87,34 @@ n = String.to_integer(arg)
 #{vet1,_} = Nx.Random.uniform(Nx.Random.key(1), shape: {1, n}, type: :f32)
 #{vet2,_} = Nx.Random.uniform(Nx.Random.key(1), shape: {1, n}, type: :f32)
 
-vet1 = Hok.new_nx_from_function(1,n,{:f,32},fn -> 1.0 end )
-vet2 = Hok.new_nx_from_function(1,n,{:f,32},fn -> 1.0 end )
+vet1 = PolyHok.new_nx_from_function(1,n,{:f,32},fn -> 1.0 end )
+vet2 = PolyHok.new_nx_from_function(1,n,{:f,32},fn -> 1.0 end )
 
 #vet1 = Nx.iota({1,n}, type: :f32)
 #vet2 = Nx.iota({1,n}, type: :f32)
 
-#vet1 = Hok.new_nx_from_function(1,n,{:f,32},fn -> :rand.uniform(1000) end )
-#vet2 = Hok.new_nx_from_function(1,n,{:f,32},fn -> :rand.uniform(1000) end)
+#vet1 = PolyHok.new_nx_from_function(1,n,{:f,32},fn -> :rand.uniform(1000) end )
+#vet2 = PolyHok.new_nx_from_function(1,n,{:f,32},fn -> :rand.uniform(1000) end)
 
-#vet1 = Hok.new_nx_from_function(1,n,{:f,32},fn -> 1.0 end )
+#vet1 = PolyHok.new_nx_from_function(1,n,{:f,32},fn -> 1.0 end )
 #vet2 = Nx.tensor([Enum.to_list(1..n)], type: {:f,32})
 
 prev = System.monotonic_time()
 
 
-ref1 = Hok.new_gnx(vet1)
+ref1 = PolyHok.new_gnx(vet1)
 
-ref2 = Hok.new_gnx(vet2)
+ref2 = PolyHok.new_gnx(vet2)
 
 
 result = ref1
-    |> DP.map2(ref2, Hok.hok fn (a,b) -> a * b end)
-    |> DP.reduce(0.0,Hok.hok fn (a,b) -> a + b end)
-    |> Hok.get_gnx
+    |> DP.map2(ref2, PolyHok.hok fn (a,b) -> a * b end)
+    |> DP.reduce(0.0,PolyHok.hok fn (a,b) -> a + b end)
+    |> PolyHok.get_gnx
 
 IO.inspect result
 
 next = System.monotonic_time()
 
 
-IO.puts "Hok\t#{n}\t#{System.convert_time_unit(next-prev,:native,:millisecond)}"
+IO.puts "PolyHok\t#{n}\t#{System.convert_time_unit(next-prev,:native,:millisecond)}"
