600d2aac4cafb3ce09b476bdfdc12965d2838fbc
ardubois <ardubois@github.com>
2025-02-07 14:04:55 -0300

    teste

--- mix.exs
+++ mix.exs
@@ -20,7 +20,8 @@ defmodule PolyHok.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
+    [   {:matrex, "~> 0.6"},
+      {:nx, "~> 0.9.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
