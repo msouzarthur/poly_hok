1444a9661c2c2e3ddb3e3f9991ed92998ce08069
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-02-11 20:05:30 -0300

    teste anonymous functions

--- benchmarks/nbodies.ex
+++ benchmarks/nbodies.ex
@@ -31,83 +31,7 @@ PolyHok.defmodule_jit NBodies do
       p[2] = p[2] + p[5]*dt;
 
   end
-  defk map_step_2_para_no_resp_kernel(d_array,  step, par1, par2,size,f) do
 
-
-    globalId  = blockDim.x * ( gridDim.x * blockIdx.y + blockIdx.x ) + threadIdx.x
-    id  = step * globalId
-    #f(id,id)
-    if (globalId < size) do
-      f(d_array+id,par1,par2)
-    end
-  end
-  def map_2_para_no_resp(d_array,  par1, par2, size, f) do
-    block_size =  128;
-    {_l,step} = PolyHok.get_shape_gnx(d_array)
-    nBlocks = floor ((size + block_size - 1) / block_size)
-
-      PolyHok.spawn_jit(&NBodies.map_step_2_para_no_resp_kernel/6,{nBlocks,1,1},{block_size,1,1},[d_array,step,par1,par2,size,f])
-      d_array
-  end
-  def nbodies(-1,p,_dt,_softening,_n) do
-    p
-  end
-  def nbodies(i,p,dt,softening,n) do
-    #p=nbodies(i-1,p,dt,softening,n)
-    {fx,fy,fz} = calc_nbodies(n,i,p,softening,0.0,0.0,0.0)
-
-    p=Matrex.set(p,1,6*i+4,Matrex.at(p,1,6*i+4)+ dt*fx);
-    p=Matrex.set(p,1,6*i+5,Matrex.at(p,1,6*i+5) + dt*fy);
-    p=Matrex.set(p,1,6*i+6,Matrex.at(p,1,6*i+6) + dt*fz);
-    nbodies(i-1,p,dt,softening,n)
-  end
-
-def calc_nbodies(-1,_i,_p,_softening,fx,fy,fz) do
-  {fx,fy,fz}
-end
-def calc_nbodies(j,i,p,softening,fx,fy,fz) do
-    dx = Matrex.at(p,1,(6*j)+1) - Matrex.at(p,1,(6*i)+1);
-    dy = Matrex.at(p,1,(6*j)+2) - Matrex.at(p,1,(6*i)+2);
-    dz = Matrex.at(p,1,(6*j)+3) - Matrex.at(p,1,(6*i)+3);
-    distSqr = dx*dx + dy*dy + dz*dz + softening;
-    invDist = 1/:math.sqrt(distSqr);
-    invDist3 = invDist * invDist * invDist;
-
-    fx = fx + dx * invDist3;
-    fy = fy + dy * invDist3;
-    fz = fz + dz * invDist3;
-    calc_nbodies(j-1,i,p,softening,fx,fy,fz)
-end
-
-def cpu_integrate(-1,p,_dt) do
-  p
-end
-def cpu_integrate(i,p, dt) do
-      p=Matrex.set(p,1,6*i+1,Matrex.at(p,1,6*i+1) + Matrex.at(p,1,6*i+4)*dt)
-      p=Matrex.set(p,1,6*i+2,Matrex.at(p,1,6*i+2) + Matrex.at(p,1,6*i+5)*dt)
-      p=Matrex.set(p,1,6*i+3,Matrex.at(p,1,6*i+3) + Matrex.at(p,1,6*i+6)*dt)
-      cpu_integrate(i-1,p,dt)
-end
-def equality(a, b) do
-  if(abs(a-b) < 0.01) do
-    true
-  else
-    false
-  end
-end
-def check_equality(0,_cpu,_gpu) do
-  :ok
-end
-def check_equality(n,cpu,gpu) do
-  gpu1 =Matrex.at(gpu,1,n)
-  cpu1 = Matrex.at(cpu,1,n)
-  if(equality(gpu1,cpu1)) do
-    check_equality(n-1,cpu,gpu)
-  else
-    IO.puts "#{n}: cpu = #{cpu1}, gpu = #{gpu1}"
-    check_equality(n-1,cpu,gpu)
-  end
-end
 end
 
 
@@ -118,27 +42,20 @@ user_value = String.to_integer(arg)
 
 
 nBodies = user_value #3000;
-#block_size =  128;
-#nBlocks = floor ((nBodies + block_size - 1) / block_size)
-#softening = 0.000000001
-#dt = 0.01; # time step
 size_body = 6
 
 
 
 h_buf = PolyHok.new_nx_from_function(nBodies,size_body,{:f,64},fn -> :rand.uniform() end )
 
-#h_buf = PolyHok.new_nx_from_function(nBodies,size_body,{:f,32},fn -> 1 end )
-
-#IO.inspect h_buf
 
 prev = System.monotonic_time()
 
 d_buf = PolyHok.new_gnx(h_buf)
 
 _gpu_resp = d_buf
-  |> NBodies.map_2_para_no_resp(d_buf,nBodies,nBodies, &NBodies.gpu_nBodies/3)
-  |> NBodies.map_2_para_no_resp( 0.01,nBodies,nBodies, &NBodies.gpu_integrate/3)
+  |> PolyHok.Ske.map(&NBodies.gpu_nBodies/3, [d_buf,nBodies], return: false)
+  |> PolyHok.Ske.map(&NBodies.gpu_integrate/3, [0.01,nBodies], return: false)
   |> PolyHok.get_gnx
   #|> IO.inspect
 
---
+++ lib/poly_hok/ske.ex
@@ -0,0 +1,30 @@
+PolyHok.defmodule_jit Ske do
+  @defaults %{coord: false, return: true}
+  def map({:nx, type, shape, name , ref}, func, [par1,par2], options \\ [])do
+    %{coord: coord, return: return} = Enum.into(options, @defaults)
+     if not coord && not return do
+      map_2_para_no_resp(d_array,  par1, par2, func)
+     end
+  end
+  defk map_step_2_para_no_resp_kernel(d_array,  step, par1, par2,size,f) do
+    globalId  = blockDim.x * ( gridDim.x * blockIdx.y + blockIdx.x ) + threadIdx.x
+    id  = step * globalId
+    #f(id,id)
+    if (globalId < size) do
+      f(d_array+id,par1,par2)
+    end
+  end
+  def map_2_para_no_resp(d_array,  par1, par2, f) do
+    block_size =  128;
+    {l,step} = PolyHok.get_shape_gnx(d_array)
+    nBlocks = floor ((size + block_size - 1) / block_size)
+
+      PolyHok.spawn_jit(&NBodies.map_step_2_para_no_resp_kernel/6,{nBlocks,1,1},{block_size,1,1},[d_array,step,par1,par2,l,f])
+      d_array
+  end
+end
+
+
+
+
+#### kernels
