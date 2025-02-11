eac334f9a963e6f34ce8cca604264a3e64219913
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-02-11 10:15:11 -0300

    teste anonymous functions

--- benchmarks/nbodies_ske.ex
+++ benchmarks/nbodies_ske.ex
@@ -140,7 +140,7 @@ _gpu_resp = d_buf
   |> NBodies.map_2_para_no_resp(d_buf,nBodies,nBodies, &NBodies.gpu_nBodies/3)
   |> NBodies.map_2_para_no_resp( 0.01,nBodies,nBodies, &NBodies.gpu_integrate/3)
   |> PolyHok.get_gnx
-  |> IO.inspect
+  #|> IO.inspect
 
   next = System.monotonic_time()
 
