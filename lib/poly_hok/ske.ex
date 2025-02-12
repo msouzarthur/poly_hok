ab12756830d28c872f09af2153b2820b92190a49
ardubois <ardubois@github.com>
2025-02-12 13:15:16 -0300

    teste anonymous functions

--- lib/poly_hok/ske.ex
+++ lib/poly_hok/ske.ex
@@ -1,12 +1,21 @@
 
-PolyHok.defmodule Ske do
+defmodule Ske do
   @defaults %{coord: false, return: true, dim: :one}
    def map({:nx, type, shape, name , ref}, func, [par1,par2], options \\ [])do
     %{coord: coord, return: return, dim: dim} = Enum.into(options, @defaults)
      if (not coord && not return && dim == :one)do
       map_2_para_no_resp(d_array,  par1, par2, func)
      end
+     def map_2_para_no_resp(d_array,  par1, par2, f) do
+      block_size =  128;
+      {l,step} = PolyHok.get_shape_gnx(d_array)
+      nBlocks = floor ((size + block_size - 1) / block_size)
+
+        PolyHok.spawn_jit(&NBodies.map_step_2_para_no_resp_kernel/6,{nBlocks,1,1},{block_size,1,1},[d_array,step,par1,par2,l,f])
+        d_array
+    end
   end
+PolyHok.defmodule SkeKernels do
   defk map_step_2_para_no_resp_kernel(d_array,  step, par1, par2,size,f) do
     globalId  = blockDim.x * ( gridDim.x * blockIdx.y + blockIdx.x ) + threadIdx.x
     id  = step * globalId
@@ -15,14 +24,7 @@ PolyHok.defmodule Ske do
       f(d_array+id,par1,par2)
     end
   end
-  def map_2_para_no_resp(d_array,  par1, par2, f) do
-    block_size =  128;
-    {l,step} = PolyHok.get_shape_gnx(d_array)
-    nBlocks = floor ((size + block_size - 1) / block_size)
 
-      PolyHok.spawn_jit(&NBodies.map_step_2_para_no_resp_kernel/6,{nBlocks,1,1},{block_size,1,1},[d_array,step,par1,par2,l,f])
-      d_array
-  end
 end
 
 
