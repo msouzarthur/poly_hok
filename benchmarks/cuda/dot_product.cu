b6978e3119a051c0ba1018884d2bdc81953c07b2
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-02-16 15:18:02 -0300

    teste anonymous functions

--- benchmarks/cuda/dot_product.cu
+++ benchmarks/cuda/dot_product.cu
@@ -2,6 +2,74 @@
 #include <time.h>
 
 
+__device__ static float atomic_cas(float* address, float oldv, float newv)
+{
+    int* address_as_i = (int*) address;
+    return  __int_as_float(atomicCAS(address_as_i, __float_as_int(oldv), __float_as_int(newv)));
+}
+
+
+
+__device__
+float anon_jmc21j2h1c(float a, float b)
+{
+return ((a * b));
+}
+
+
+extern "C" __global__ void map_2kernel(float *a1, float *a2, float *a3, int size)
+{
+	int id = ((blockIdx.x * blockDim.x) + threadIdx.x);
+if((id < size))
+{
+	a3[id] = anon_jmc21j2h1c(a1[id], a2[id]);
+}
+
+}
+
+
+
+
+__device__
+float anon_akgfg4f0hl(float a, float b)
+{
+return ((a + b));
+}
+
+
+extern "C" __global__ void reduce_kernel(float *a, float initial, float *ref4, int n)
+{
+__shared__ float cache[256];
+	int tid = (threadIdx.x + (blockIdx.x * blockDim.x));
+	int cacheIndex = threadIdx.x;
+	float temp = initial;
+while((tid < n)){
+	temp = anon_akgfg4f0hl(a[tid], temp);
+	tid = ((blockDim.x * gridDim.x) + tid);
+}
+	cache[cacheIndex] = temp;
+__syncthreads();
+	int i = (blockDim.x / 2);
+while((i != 0)){
+if((cacheIndex < i))
+{
+	cache[cacheIndex] = anon_akgfg4f0hl(cache[(cacheIndex + i)], cache[cacheIndex]);
+}
+
+__syncthreads();
+	i = (i / 2);
+}
+if((cacheIndex == 0))
+{
+	float current_value = ref4[0];
+while((! (current_value == atomic_cas(ref4, current_value, anon_akgfg4f0hl(cache[0], current_value))))){
+	current_value = ref4[0];
+}
+}
+
+}
+
+
 
 
 
@@ -54,14 +122,14 @@ int main(int argc, char *argv[])
      j_error = cudaGetLastError();
     if(j_error != cudaSuccess) {printf("Error: %s\n", cudaGetErrorString(j_error)); exit(1);}
 
-    float (*f1)(float,float) = (float (*)(float,float)) get_anonymous_mult_ptr();
-    float (*f2)(float,float) = (float (*)(float,float)) get_anonymous_sum_ptr();
+   // float (*f1)(float,float) = (float (*)(float,float)) get_anonymous_mult_ptr();
+    //float (*f2)(float,float) = (float (*)(float,float)) get_anonymous_sum_ptr();
 
     float *final, *d_final;
     final = (float *)malloc(sizeof(float));
 	cudaMalloc((void **) &d_final,sizeof(float));
 
-    map_2kernel<<< numberOfBlocks, threadsPerBlock>>>(dev_a, dev_b, dev_resp, N, f1);
+    map_2kernel<<< numberOfBlocks, threadsPerBlock>>>(dev_a, dev_b, dev_resp, N);
      j_error = cudaGetLastError();
     if(j_error != cudaSuccess) {printf("Error: %s\n", cudaGetErrorString(j_error)); exit(1);}
 
