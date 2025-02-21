9321c1ca8e0323cc9a3d36f0bb4f9548b81f41f9
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-02-21 10:46:21 -0300

    teste anonymous functions

--- c_src/Elixir.CAS_Double.cu
+++ c_src/Elixir.CAS_Double.cu
@@ -2,7 +2,7 @@
 __device__ static double atomic_cas(double* address, double oldv, double newv)
 {
     unsigned long long int * address_as_i = (unsigned long long int *) address;
-    return  __longlong_as_double(atomicCAS(address_as_i, (__double_as_longlong(oldv),__double_as_longlong(newv)));
+    return  __longlong_as_double(  atomicCAS(address_as_i, __double_as_longlong(oldv),__double_as_longlong(newv))  );
 }
 
 
