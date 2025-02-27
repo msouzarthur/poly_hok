4897312e30da487d2dd25196e3307d1398c04636
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-02-27 10:28:25 -0300

    teste anonymous functions

--- benchmarks/without_skeletons/mm.ex
+++ benchmarks/without_skeletons/mm.ex
@@ -29,8 +29,14 @@ block_size = 16
 grid_rows = trunc ((m + block_size - 1) / block_size)
 grid_cols = trunc ((k + block_size - 1) / block_size)
 
-mat1 = PolyHok.new_nx_from_function(1,m*m,{:f,32},fn -> :rand.uniform(1000) end )
-mat2 = PolyHok.new_nx_from_function(1,m*m,{:f,32},fn -> :rand.uniform(1000) end)
+#mat1 = PolyHok.new_nx_from_function(1,m*m,{:f,32},fn -> :rand.uniform(1000) end )
+#mat2 = PolyHok.new_nx_from_function(1,m*m,{:f,32},fn -> :rand.uniform(1000) end)
+
+mat1 = Nx.tensor(Enum.to_list(1..(m*m)), type: :f32)
+mat2 = Nx.tensor(Enum.to_list(1..(m*m)),  type: :f32)
+
+mat1 = Nx.reshape(mat1,{m,m})
+mat2 = Nx.reshape(mat2,{m,m})
 
 prev = System.monotonic_time()
 
@@ -40,7 +46,13 @@ c=PolyHok.new_gnx(1,m*k, {:f,32})
 
 PolyHok.spawn(&MM.mm/6,{grid_rows,grid_cols,1},{block_size,block_size,1},[a,b,c,m,n,k])
 
-_result = PolyHok.get_gnx(c)
+result = PolyHok.get_gnx(c)
+
+r2 = Nx.dot(mat1,mat2)
+
+IO.inspect Nx.equal(result,r2)
+IO.inspect result
+IO.inspect r2
 
 next = System.monotonic_time()
 #IO.puts "time gpu #{System.convert_time_unit(next-prev,:native,:millisecond)}"
