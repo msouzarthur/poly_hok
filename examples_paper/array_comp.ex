acb4dd343f69ee4992c475dbd5b38640d3b97fa9
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-03-04 14:38:34 -0300

    teste anonymous functions

--- examples_paper/array_comp.ex
+++ examples_paper/array_comp.ex
@@ -1,5 +1,36 @@
 require PolyHok
 
+
+PolyHok.defmodule PMap do
+  defk map_ker(a1,a2,size,f) do
+    id = blockIdx.x * blockDim.x + threadIdx.x
+    stride = blockDim.x * gridDim.x
+
+    for i in range(index,size,stride) do
+          a2[id] = f(a1[id])
+    end
+  end
+  defd inc(x) do
+    x+1
+  end
+  def map(input, f) do
+    shape = PolyHok.get_shape(input)
+    type = PolyHok.get_type(input)
+    result_gpu = PolyHok.new_gnx(shape,type)
+
+    size = Tuple.product(shape)
+    threadsPerBlock = 128;
+    numberOfBlocks = div(size + threadsPerBlock - 1, threadsPerBlock)
+
+    PolyHok.spawn(&PMap.map_ske/4,
+              {numberOfBlocks,1,1},
+              {threadsPerBlock,1,1},
+              [input,result_gpu,size, f])
+    result_gpu
+  end
+
+end
+
 host_array =  tensor1 = Nx.tensor([[1,2,3,4]],type: {:s, 32})
 
 host_resp = PolyHok.gpu_for n <- host_array,  do: n * n
