d35e0d3da11dbfdf84e2c73118d4358f5d3274db
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-03-04 14:40:32 -0300

    teste anonymous functions

--- examples_paper/array_comp.ex
+++ examples_paper/array_comp.ex
@@ -31,7 +31,7 @@ PolyHok.defmodule PMap do
 
 end
 
-host_array =  tensor1 = Nx.tensor([[1,2,3,4]],type: {:s, 32})
+host_array = Nx.tensor([[1,2,3,4]],type: {:s, 32})
 
 host_resp = PolyHok.gpu_for n <- host_array,  do: n * n
 
