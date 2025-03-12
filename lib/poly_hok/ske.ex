1ee3128bcdcb7b3b8699db85b8274156e8adfbb5
ardubois <ardubois@github.com>
2025-03-12 14:20:22 -0300

    teste anonymous functions

--- lib/poly_hok/ske.ex
+++ lib/poly_hok/ske.ex
@@ -12,7 +12,7 @@ PolyHok.defmodule SkeKernels do
 
 end
 
-defmodule PolyHok.Ske do
+PolyHok.defmodule PolyHok.Ske do
   @defaults %{coord: false, return: true, dim: :one}
    def map({:nx, type, shape, name , ref}, func, [par1,par2], options \\ [])do
     %{coord: coord, return: return, dim: dim} = Enum.into(options, @defaults)
