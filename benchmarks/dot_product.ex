75498de82c7036339f9f2d531995de1b5658797b
ardubois <ardubois@github.com>
2025-03-12 13:40:19 -0300

    teste anonymous functions

--- benchmarks/dot_product.ex
+++ benchmarks/dot_product.ex
@@ -87,8 +87,8 @@ n = String.to_integer(arg)
 #{vet1,_} = Nx.Random.uniform(Nx.Random.key(1), shape: {1, n}, type: :f32)
 #{vet2,_} = Nx.Random.uniform(Nx.Random.key(1), shape: {1, n}, type: :f32)
 
-vet1 = PolyHok.new_nx_from_function(1,n,{:f,32},fn -> 1.0 end )
-vet2 = PolyHok.new_nx_from_function(1,n,{:f,32},fn -> 1.0 end )
+vet1 = PolyHok.new_nx_from_function(1,n,{:f,32},fn -> 0.1 end )
+vet2 = PolyHok.new_nx_from_function(1,n,{:f,32},fn -> 0.1 end )
 
 #vet1 = Nx.iota({1,n}, type: :f32)
 #vet2 = Nx.iota({1,n}, type: :f32)
