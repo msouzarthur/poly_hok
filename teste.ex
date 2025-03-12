0ee69d1fbc44789851899e74f0c4131df9170eeb
ardubois <ardubois@github.com>
2025-03-12 14:41:11 -0300

    teste anonymous functions

--- teste.ex
+++ teste.ex
@@ -6,7 +6,8 @@ n = 1000
 arr1 = Nx.tensor([Enum.to_list(1..n)],type: {:s, 32})
 
 
-host_res1 = arr1
+arr1
     |> PolyHok.new_gnx
     |> Ske.map(PolyHok.phok fn (x) -> x + 1 end)
     |> PolyHok.get_gnx
+    |> IO.inspect
