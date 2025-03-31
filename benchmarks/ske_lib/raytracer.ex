6de897705a7bad0d43a6bd0b83afc9ab87c072ee
ardubois <ardubois@github.com>
2025-03-31 13:26:42 -0300

    generic commit message

--- benchmarks/ske_lib/raytracer.ex
+++ benchmarks/ske_lib/raytracer.ex
@@ -179,7 +179,7 @@ defmodule Main do
         ref_sphere = PolyHok.new_gnx(sphereList)
         ref_image = PolyHok.new_gnx(1,width * height  * 4,{:s,32})
 
-        Ske.map(ref_image, &RayTracer.raytracing/5 , [width, spheres], dim: :two, return: false)
+        Ske.map(ref_image, &RayTracer.raytracing/5 , [width, sphereList], dim: :two, return: false)
       
         _image = PolyHok.get_gnx(ref_image)
 
